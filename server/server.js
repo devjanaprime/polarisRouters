14b2025e22653754765b3bf35e4c49c2314bc191
Dev Jana <dev@primeacademy.io>
2018-08-03 11:14:52 -0500

    server.js route review

--- server/server.js
+++ server/server.js
@@ -9,8 +9,14 @@ app.use( bodyParser.urlencoded( { extended: true } ) );
 
 // globals
 const port = 5000;
+let candies = [ { name:'skittles', flavors: [ 'strawberry', 'apple(green)', 'lemon', 'grape', 'orange' ] }]
 
 // server
 app.listen( port, ()=>{
     console.log( 'server up on:', port );
 }) // end server up
+
+app.get( '/candies', ( req, res )=>{
+    console.log( '/candies GET route hit' )
+    res.send( candies );
+})
